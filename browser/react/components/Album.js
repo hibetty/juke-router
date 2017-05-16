606ec4c7e854793c682552582669e5722e62aead
Betty Lin <bettyhappylin@gmail.com>
2017-05-16 17:19:34 -0400

    complete section 3 "contructing a router"

--- browser/react/components/Album.js
+++ browser/react/components/Album.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import Songs from '../components/Songs';
 
-const Album = (props) => {
+export default class Album extends React.Component {
+  componentDidMount(){
+    const albumId = this.props.routeParams.albumId;
+    const selectAlbum = this.props.selectAlbum;
 
-  const album = props.album;
-  const currentSong = props.currentSong;
-  const isPlaying = props.isPlaying;
-  const toggleOne = props.toggleOne;
+    selectAlbum(albumId);
 
-  return (
-    <div className="album">
-      <div>
-        <h3>{ album.name }</h3>
-        <img src={ album.imageUrl } className="img-thumbnail" />
+  }
+
+  render() {
+    const album = this.props.album;
+    const currentSong = this.props.currentSong;
+    const isPlaying = this.props.isPlaying;
+    const toggleOne = this.props.toggleOne;
+    return (
+      <div className="album">
+        <div>
+          <h3>{ album.name }</h3>
+          <img src={ album.imageUrl } className="img-thumbnail" />
+        </div>
+        <Songs
+          songs={album.songs}
+          currentSong={currentSong}
+          isPlaying={isPlaying}
+          toggleOne={toggleOne} />
       </div>
-      <Songs
-        songs={album.songs}
-        currentSong={currentSong}
-        isPlaying={isPlaying}
-        toggleOne={toggleOne} />
-    </div>
-  );
-}
+    );
+  }
 
-export default Album;
+}
