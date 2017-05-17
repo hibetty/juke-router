a3a8622b4aa45143a8ef01e2b7c4a35b304fd674
Jing Wang <jwang2048@gmail.com>
2017-05-17 11:57:21 -0400

    completed subroutes

--- browser/react/components/Artist.js
+++ browser/react/components/Artist.js
@@ -7,6 +7,7 @@ export default class Artist extends React.Component {
   componentDidMount(){
     const artistId = this.props.routeParams.artistId;
     const selectArtist = this.props.selectArtist;
+    
 
     selectArtist(artistId);
 
@@ -17,16 +18,31 @@ export default class Artist extends React.Component {
     const albums = this.props.artistAlbums;
     const songs = this.props.artistSongs;
     const currentSong = this.props.currentSong;
+    const children = this.props.children;
+    const toggleOne = this.props.toggleOne;
+    const isPlaying = this.props.isPlaying;
+    const propsToPassToChildren = {
+      /**todo: make sure to include all the props that the child components need! **/
+      artist, albums, songs, currentSong, toggleOne, isPlaying
+    }
 
 		return (
 			<div>
 			  <h3>{artist.name}</h3>
+        <ul className="nav nav-tabs">
+          <li><Link to={`/artists/:artistId/albums`}>ALBUMS</Link></li>
+          <li><Link to={`/artists/:artistId/songs`}>SONGS</Link></li>
+        </ul>
+        { children && React.cloneElement(children, propsToPassToChildren) }
 
+
+        {/*
         <Albums albums={albums} />
         <Songs songs={songs}
           currentSong={currentSong}
           isPlaying={this.props.isPlaying}
           toggleOne={this.props.toggleOne} />
+        */}
 
 			</div>
 			)
--- browser/react/index.js
+++ browser/react/index.js
@@ -6,6 +6,7 @@ import Albums from './components/Albums';
 import Album from './components/Album';
 import Artists from './components/Artists';
 import Artist from './components/Artist';
+import Songs from './components/Songs';
 
 ReactDOM.render(
   <Router history={hashHistory}>
@@ -14,8 +15,10 @@ ReactDOM.render(
       <Route path='/albums' component={Albums} />
       <Route path="albums/:albumId" component={Album} />
       <Route path="/artists" component={Artists} />
-      <Route path="/artists/:artistId" component={Artist} />
-
+      <Route path="/artists/:artistId" component={Artist}>
+        <Route path="albums" component={Albums} />
+        <Route path="songs" component={Songs} />
+      </Route>
     </Route>
   </Router>,
   document.getElementById('app')
